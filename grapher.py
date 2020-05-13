f08677440710de803eafc506ee2571148476e371
Sergey Kovalev <skovalev@anchorfree.com>
2020-05-14 00:04:03 +0700

    Add simple grapher

---
+++ grapher.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import time
+import sys
+
+def usage():
+  print("Usage: grapher.py <filename1> <filename2> ...")
+  exit(1)
+
+if len(sys.argv) == 1:
+  usage()
+
+def source(filename):
+  with open(filename, encoding='cp1251') as f:
+    for l in f:
+      kws = l.split(',')
+      t = kws[0]
+      if t == '$GNRMC':
+        time = kws[1]
+        h = int(time[0:2])
+        m = int(time[2:4])
+        s = int(time[4:6])
+        ms = int(time[7:9]) * 10
+        ltime = h * 3600 + m * 60 + s + ms/1000
+        status = kws[2]
+        svel = kws[7]
+        if not svel:
+          svel = '0'
+        vel = float(svel) * 1.85
+        yield ltime, vel
+
+for filename in sys.argv[1:]:
+  print(filename)
+  dataX = []
+  dataY = []
+  startX = None
+  for now,vel in source(filename):
+    if len(dataX) == 0:
+      dataX.append(0)
+      startX = now
+    else:
+      dataX.append(now - startX)
+    dataY.append(vel)
+  plt.plot(dataX, dataY)
+
+plt.show()
